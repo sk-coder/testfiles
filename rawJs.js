e84deb3f856dcf7cea309b73915463f2497cf516
Scott Karneth <sk954@outlook.com>
2019-12-11 11:55:22 -0500

    added raw file, fixed minor js errors

---
+++ rawJs.js
@@ -0,0 +1,12 @@
+ceq.log("Running RawJs.js");
+var myValue = 1;
+ceq.log("Set myValue to: " + myValue);
+
+if ( myValue == 1 )
+{
+  ceq.log("Checked that value was 1");
+}
+else
+{
+  ceq.log("The IF did NOT work!!!!");
+}
