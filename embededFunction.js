c3771bd515d88cae3d6ff199d1185139481ee221
sk-coder <sk954@outlook.com>
2019-12-11 11:17:10 -0500

    Create embededFunction.js

---
+++ embededFunction.js
@@ -0,0 +1,14 @@
+(function (){
+  ceq_core.logger.logD("Running embededFunction.js");
+  var myValue = 1;
+  ceq.log("Set myValue to: " + myValue);
+
+  if ( myValue == 1 )
+  {
+    ceq.log("Checked that value was 1";
+  }
+  else
+    ceq.log("The IF did NOT work!!!!");
+  }
+
+})();
